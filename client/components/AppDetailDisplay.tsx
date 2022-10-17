a040c4e87f154ec68ce9e8b90f21dd0212fc9a99
sagarvxyz <sagar.velagala@gmail.com>
2022-10-17 16:25:29 -0400

    feat: display component templates

---
+++ client/components/AppDetailDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AppDetailDisplay = () => {
+  return (
+    <div>AppDetailDisplay</div>
+  );
+};
+
+export default AppDetailDisplay;
---
+++ client/components/HeaderDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HeaderDisplay = () => {
+  return (
+    <div>HeaderDisplay</div>
+  );
+};
+
+export default HeaderDisplay;
