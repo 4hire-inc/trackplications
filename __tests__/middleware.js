9071383ff7d8491e96b316ce447353f8ac783899
Sandy <icynxx@gmail.com>
2022-10-18 12:28:21 -0400

    edits on database

---
+++ __tests__/middleware.js
@@ -0,0 +1,17 @@
+import applicationController from '../server/controllers/applicationController';
+
+// describe('applicationController middleware unit tests', () => {
+//   let Request;
+//   let Response;
+//   let nextFunction;
+
+//   describe('getApplication middleware', () => {
+//     beforeEach(() => {
+//       Request = {};
+//       Response = {}; 
+//       nextFunction = jest.fn();
+//     });
+//   });
+
+//   it('')
+// });
