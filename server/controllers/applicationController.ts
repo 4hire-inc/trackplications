f5148d6519be323c1a9cdcb3dcf73d8bcf382a50
samos17 <amos.serena17@gmail.com>
2022-10-18 15:58:47 -0400

    refine delete application route

--- server/controllers/applicationController.ts
+++ server/controllers/applicationController.ts
@@ -4,17 +4,16 @@ import { ApplicationController } from '../serverTypes';
 const applicationController: ApplicationController = {
 // middleware to get all applications
   getApplications: async (req: any, res, next) => {
-    const userId = req.user.id;
     try {
       const queryString = `
       SELECT *
       FROM applications AS a, users AS u, offers AS o, status AS s
-      WHERE a.user_id = ($1)
+      WHERE a.user_id = o.user_id
       AND a.status_id = s.id
       AND a.offer_id = o.id
       `;
       // const result: Array<any> = await applicationModel.query(queryString, undefined, () => console.log('test'));
-      applicationModel.query(queryString, [userId], (err, result) => {
+      applicationModel.query(queryString, undefined, (err, result) => {
         if (err) return next({ err });
         console.log('result:', result?.rows);
         res.locals.applications = result?.rows;
@@ -30,12 +29,10 @@ const applicationController: ApplicationController = {
   },
   deleteApplication: (req: any, res, next) => {
     const appId = req.params.id;
-    let userId = req.user?.id;
-    console.log(userId);
-    if (!userId) userId = 'Z0CTnEeAPm';
-
-    const deleteStatusQuery = `DELETE FROM status WHERE id='${appId}'`;
-    const deleteOfferQuery = `DELETE FROM offers WHERE id='${appId}'`;
+    const userId = req.user?.id;
+    
+    const deleteStatusQuery = `DELETE FROM status WHERE app_id='${appId}'`;
+    const deleteOfferQuery = `DELETE FROM offers WHERE app_id='${appId}'`;
 
     const deleteApplicationQuery = 'DELETE FROM applications WHERE id=($1) AND user_id=($2)';
     const params = [appId, userId];
@@ -59,15 +56,17 @@ const applicationController: ApplicationController = {
               message: { error: 'Error in applicationController: deleteApplication (delete application)' },
               status: 500,
             });
+            else {
+              res.locals.deleted = {
+                id: appId,
+                confirmation: true,
+              };
+              return next();
+            }
           });
         }
       });
     });
-    res.locals.deleted = {
-      id: appId,
-      confirmation: true,
-    };
-    return next();
   }
 
 }; 
